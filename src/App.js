71e530bb205c17d015c9cd57947698fd401fde13
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2023-08-23 15:44:30 +0200

    Change App.js and index.js. Remove files. Add Stoper.js and Stoper.module.scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Stoper from './components/Stoper/Stoper'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>Stoper App</h2>
       </header>
+      <Stoper />
     </div>
   );
 }
---
+++ src/components/Stoper/Stoper.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+const Stoper = () => {
+
+  const [originAirport, setOriginAirport] = useState('');
+
+  return (
+    <div>
+      <h2>Stoper component</h2>
+    </div>
+  );
+}
+
+export default Stoper;
